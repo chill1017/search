c7f6f30a3544e23e8077a36143754e0fa39f717e
Caleb Hill <ckh1017@wildcats.unh.edu>
2025-08-27 15:17:23 -0400

    migrated some more to util

--- puzzle_utilities.py
+++ puzzle_utilities.py
@@ -1,11 +1,10 @@
 import numpy as np
-import random
 
 # enter as a vector. reshape and hold as a matrix
 class puzzle_state:
     def __init__(self, config):
         conf1 = config
-        self.config = conf1.reshape(len(config),len(config))
+        self.config = conf1.reshape(int(np.sqrt(len(config))),int(np.sqrt(len(config))))
         self.parent = None
         self.d = 0
 
@@ -20,14 +19,26 @@ class puzzle_state:
         new.d = self.d
         return new
     
+    # chatGPT output
+    def __eq__(self, other):
+        if not isinstance(other, puzzle_state):
+            return False
+        return np.array_equal(self.config, other.config)
+
+    def __hash__(self):
+        # Convert config to an immutable representation for hashing
+        return hash(tuple(self.config.flatten()))
+    # chatGPT output end
+    
 # find the neighbors of a puzzle_state
-def moves(par_state: puzzle_state):                         
+def moves(par_state: puzzle_state):  
+    side_len = par_state.config.shape[0]                       
     a = par_state.config
     i = np.argwhere(a == 0)[0][0]
     j = np.argwhere(a == 0)[0][1]
 
     # center
-    if (i!=0 and i!=SIDE-1) and (j!=0 and j!=SIDE-1):
+    if (i!=0 and i!=side_len-1) and (j!=0 and j!=side_len-1):
         u = par_state.copy()
         d = par_state.copy()
         l = par_state.copy()
@@ -77,7 +88,7 @@ def moves(par_state: puzzle_state):
         r.d = par_state.d + 1
         d.d = par_state.d + 1
         return [r,d]
-    elif i==0 and j==SIDE-1:
+    elif i==0 and j==side_len-1:
         d = par_state.copy()
         l = par_state.copy()
         key = d.config[i+1,j]
@@ -94,7 +105,7 @@ def moves(par_state: puzzle_state):
         l.d = par_state.d + 1
         d.d = par_state.d + 1
         return [l,d]
-    elif i==SIDE-1 and j==0:
+    elif i==side_len-1 and j==0:
         u = par_state.copy()
         r = par_state.copy()
         key = u.config[i-1,j]
@@ -111,7 +122,7 @@ def moves(par_state: puzzle_state):
         u.d = par_state.d + 1
         r.d = par_state.d + 1
         return [u,r]
-    elif i==SIDE-1 and j==SIDE-1:
+    elif i==side_len-1 and j==side_len-1:
         u = par_state.copy()
         l = par_state.copy()
         key = u.config[i-1,j]
@@ -130,7 +141,7 @@ def moves(par_state: puzzle_state):
         return [u,l]
 
     # non-corner edges
-    elif i==0 and(j!=0 and j!=SIDE-1):
+    elif i==0 and(j!=0 and j!=side_len-1):
         d = par_state.copy()
         l = par_state.copy()
         r = par_state.copy()
@@ -152,7 +163,7 @@ def moves(par_state: puzzle_state):
         r.d = par_state.d + 1
         d.d = par_state.d + 1
         return [l,r,d]
-    elif i==SIDE-1 and(j!=0 and j!=SIDE-1):
+    elif i==side_len-1 and(j!=0 and j!=side_len-1):
         u = par_state.copy()
         l = par_state.copy()
         r = par_state.copy()
@@ -174,7 +185,7 @@ def moves(par_state: puzzle_state):
         r.d = par_state.d + 1
         u.d = par_state.d + 1
         return [l,r,u]
-    elif (i!=0 and i!=SIDE-1) and j==0:
+    elif (i!=0 and i!=side_len-1) and j==0:
         u = par_state.copy()
         d = par_state.copy()
         r = par_state.copy()
@@ -196,7 +207,7 @@ def moves(par_state: puzzle_state):
         d.d = par_state.d + 1
         r.d = par_state.d + 1
         return [u,d,r]
-    elif (i!=0 and i!=SIDE-1) and j==SIDE-1:
+    elif (i!=0 and i!=side_len-1) and j==side_len-1:
         u = par_state.copy()
         d = par_state.copy()
         l = par_state.copy()
@@ -219,13 +230,33 @@ def moves(par_state: puzzle_state):
         l.d = par_state.d + 1
         return [u,d,l]
     
+def is_found(found_list: list, to_check: puzzle_state):
+    for s in found_list:
+        if np.array_equal(s.config, to_check.config):
+            return True
+    
+    return False
 
+def manhattan_total(arr):
+    sum = 0
+    side = arr.shape[0]
+    for i in range(side):
+        for j in range(side):
+            val = arr[i, j]
+            if val != 0:
+                sum = sum + np.abs( int((val-1)/side) - i) + np.abs( ((val-1)%side) - j)
+    return sum
 
+def horz_inv(arr):
+    sum = 0
+    side = arr.shape[0]
+    flat = arr.copy()
+    flat = arr.reshape(side**2,)
+    for i in range(side**2-1):
+        for j in range(i+1,side**2):
+            if (flat[i]>flat[j]) and (flat[j]!=0):
+                sum = sum + 1
+    return sum
 
-def random_state():
-    here = puzzle_state(HOME)
-    for i in range(100):
-        here = random.choice(moves(here))
-    here.parent = None
-    here.d = 0
-    return here
+def inversion_dist(arr):
+    return horz_inv(arr) + horz_inv(arr.transpose())
